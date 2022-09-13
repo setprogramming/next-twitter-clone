e2ac1d46822e6d2cd8e01ab6b926ad3ed19efa69
Set Programming <setprogramming@gmail.com>
2022-09-13 10:31:01 +0300

    added animation effects using Framer Motion

--- components/Feed.js
+++ components/Feed.js
@@ -4,6 +4,7 @@ import Post from "./Post"
 import { useEffect, useState } from "react"
 import { onSnapshot, query, collection, orderBy} from "firebase/firestore"
 import { db} from "../firebase"
+import { AnimatePresence, motion } from "framer-motion"
 
 export default function Feed() {
   const [posts, setPosts] = useState([])
@@ -48,9 +49,20 @@ export default function Feed() {
 
         <Input />
 
-        {posts.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
+        <AnimatePresence>
+          {posts.map((post) => (
+            <motion.div 
+              key={post.id} 
+              initial={{ opacity: 0 }} 
+              animate={{ opacity: 1 }} 
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1 }}
+            >              
+              <Post key={post.id} post={post} />
+            </motion.div>
+          ))}
+        </AnimatePresence>
+        
     </div>
   )
 }
--- components/News.js
+++ components/News.js
@@ -3,7 +3,8 @@
 export default function News({article}) {
   return (
     <a href={article.url} target="_blank">
-        <div className="flex items-center justify-between px-4 py-2 space-x-1 hover:bg-gray-200 transition duration-200">
+        <div className="flex items-center justify-between px-4 py-2 space-x-1 hover:bg-gray-200
+         transition duration-500 ease-out">
             <div className="">
                 <h6 className="text-sm font-bold text-gray-600">{article.title}</h6>                
             </div>
